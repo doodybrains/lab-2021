13c4f496b0492dd856c0e30d36e98c87c0e96044
emma rae <doodybrainz@gmail.com>
2021-04-15 21:30:32 -0400

    wip

--- debug-1/script.js
+++ debug-1/script.js
@@ -1,16 +1,19 @@
 // here i added a console log just to make sure my javascript file is connected to my HTML file
 console.log("my js is working!!!!!");
 
-// this line get all elements that have class name of bubble container
-
-
+// here i have written a new function, i called it putBubblesEverywhere
 function putBubblesEverywhere() {
+  // this line get all elements that have class name of bubble container
   let allBubbleContainers = document.querySelectorAll(".bubble");
   // this line uses the allBubbleContainers variable and attaches a forEach function to it. a forEach function is a way to go through EACH of the bubble container elements on my page. in this example ive pushed in the variable bubble so that i can use it to do stuff to EACH bubble container inside the function
   allBubbleContainers.forEach(function(bubble) {
+    // here I am getting a random number between 0 and the width of the window
     let randomHorizontalPosition = Math.floor(Math.random() * window.innerWidth);
+    // here I am getting a random number between 0 and the height of the window
     let randomVerticalPosition = Math.floor(Math.random() * window.innerHeight);
+    // here i am setting the top value of each bubble to the random vertical position and adding px!
     bubble.style.top = randomVerticalPosition + "px";
+    // here i am setting the top value of each bubble to the random horizontal position and adding px!
     bubble.style.left = randomHorizontalPosition + "px";
     // this line uses the bubble variable and adds an event listener, an event listener is a javascript function that listens for user interactions! in this case the interaction is a CLICK
     bubble.addEventListener("click", event => {
@@ -23,11 +26,12 @@ function putBubblesEverywhere() {
 }
 
 
-
+//we will do these comments in class together!
 document.addEventListener('keydown', addBubble);
 
 function addBubble(event) {
   console.log(event);
+
   if (event.key === "b") {
     let newBubble = document.createElement("div");
     newBubble.classList.add("bubble");
@@ -35,6 +39,5 @@ function addBubble(event) {
     putBubblesEverywhere();
   }
 }
-
 
 putBubblesEverywhere();
